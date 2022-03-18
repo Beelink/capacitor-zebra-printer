4a76ebf6f878abc8e5c45edebcfb5f54702b3ed0
Eugene <40366303+Beelink@users.noreply.github.com>
2022-03-18 13:56:13 +0200

    Add print() realisation for iOS

--- ios/Plugin/ZebraPrinterPlugin.swift
+++ ios/Plugin/ZebraPrinterPlugin.swift
@@ -1,18 +1,38 @@
 import Foundation
 import Capacitor
+import SwiftSocket
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(ZebraPrinterPlugin)
 public class ZebraPrinterPlugin: CAPPlugin {
-    private let implementation = ZebraPrinter()
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.resolve([
-            "value": implementation.echo(value)
+            "value": value
         ])
+    }
+
+    @objc func print(_ call: CAPPluginCall) {
+        let ip = call.getString("ip") ?? ""
+        let port = call.getInt("port") ?? 0
+        let zpl = call.getString("zpl") ?? ""
+
+        let client:TCPClient = TCPClient(address: ip, port: Int32(port))
+        switch client.connect(timeout: 30){
+            case .success:
+                switch client.send(string: zpl) {
+                    case .success:
+                        guard let data = client.read(1024*10)
+                        else {
+                            call.resolve()
+                            return
+                        }
+                    case .failure(let error):
+                        call.reject("Something went wrong")
+
+                }
+            case .failure(let error):
+                call.reject("Something went wrong")
+        }
     }
 }
